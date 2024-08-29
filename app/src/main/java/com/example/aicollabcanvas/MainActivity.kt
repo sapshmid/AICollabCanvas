db6d18c4ca6547dce416ff3bd5e6ad83ac8cf341
ShellyRachminov <sheli14789@gmail.com>
2024-08-29 17:47:21 +0300

    edit and new fragment Register

--- app/src/main/java/com/example/aicollabcanvas/MainActivity.kt
+++ app/src/main/java/com/example/aicollabcanvas/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.aicollabcanvas
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.aicollabcanvas.ui.login.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
+
