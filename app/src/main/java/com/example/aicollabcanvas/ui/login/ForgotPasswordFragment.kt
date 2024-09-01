e5f4975ed717cf93b994ed8bba65c8a15eccd90a
ShellyRachminov <sheli14789@gmail.com>
2024-09-02 01:34:27 +0300

    added forgot password fragment

---
+++ app/src/main/java/com/example/aicollabcanvas/ui/login/ForgotPasswordFragment.kt
@@ -0,0 +1,20 @@
+package com.example.aicollabcanvas.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.aicollabcanvas.R
+
+class ForgotPasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        return view
+    }
+}
